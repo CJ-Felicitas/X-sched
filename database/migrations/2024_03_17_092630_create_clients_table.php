8ae792f19b972642dd1605306694f5fce29e1f00
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-03-17 22:24:13 +0800

    created a clients table migration

---
+++ database/migrations/2024_03_17_092630_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('guardian_first_name');
+            $table->string('guardian_middle_name');
+            $table->string('guardian_last_name');
+            $table->integer('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};

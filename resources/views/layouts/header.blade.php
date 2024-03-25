0ccb2d0f0a083cd586b58aec67c8dc13b0be94ca
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-03-26 01:36:07 +0800

    adjusted the path of the assets

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="uikit/img/logo/logo.png" rel="icon">
+    <link href="/uikit/img/logo/logo.png" rel="icon">
     <title>Dashboard</title>
-    <link href="uikit/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="uikit/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="uikit/css/ruang-admin.min.css" rel="stylesheet">
+    <link href="/uikit/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/uikit/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="/uikit/css/ruang-admin.min.css" rel="stylesheet">
   </head>
   

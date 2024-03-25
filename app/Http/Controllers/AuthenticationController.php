2f759209711ffb456c5463c12f64fd5af56e4091
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-03-26 01:53:02 +0800

    fixed the authenticator

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Enums\UserTypeEnum;
 class AuthenticationController extends Controller
 {
     public function login(Request $request)
@@ -12,8 +12,14 @@ class AuthenticationController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->user_type_id == UserTypeEnum::Administrator ) {
+                return redirect('/admin/dashboard');
+            } elseif ($user->user_type_id == UserTypeEnum::Staff) {
+                return redirect('/staff/managepatient');
+            }
             // Authentication passed
-            return redirect('/dashboard'); 
+            return view('/dashboard'); 
         }
         // Authentication failed
         return redirect('/')->with('error', 'Invalid credentials');
